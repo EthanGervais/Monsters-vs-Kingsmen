02d17746d12518761de609e63f5867acb4b121fe
Dawson MacPhee <dmacphee@uoguelph.ca>
2020-10-19 21:19:21 -0400

    Finshed updating dependencies

---
+++ MonstersVsKingsmen/src/main/java/plugin/monstersvskingsmen/MonstersVsKingsmen.java
@@ -0,0 +1,15 @@
+package plugin.monstersvskingsmen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MonstersVsKingsmen extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
