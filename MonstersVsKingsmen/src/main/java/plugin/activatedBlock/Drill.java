0c504adfb488a4bf70a8f814009732aa6d5f2575
Dawson MacPhee <dmacphee@uoguelph.ca>
2020-10-20 23:26:01 -0400

    Updated BakerClass

--- MonstersVsKingsmen/src/main/java/plugin/activatedBlock/Drill.java
+++ MonstersVsKingsmen/src/main/java/plugin/activatedBlock/Drill.java
@@ -1,5 +1,5 @@
 package plugin.activatedBlock;
 
 public class Drill {
-
+	
 }
--- MonstersVsKingsmen/src/main/java/plugin/classes/BakerClass.java
+++ MonstersVsKingsmen/src/main/java/plugin/classes/BakerClass.java
@@ -1,17 +1,5 @@
 package plugin.classes;
 
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
 public class BakerClass {
-	public void giveItems(Player player) {
-		Inventory inventory = player.getInventory();
-		ItemStack stack1 = new ItemStack(Material.COBBLESTONE, 64);
-		ItemStack stack2 = new ItemStack(Material.STONECUTTER, 1);
-		inventory.remove(Material.ENDERMAN_SPAWN_EGG);
-		inventory.addItem(stack1);
-		inventory.addItem(stack2);
-	} 
+	
 }
