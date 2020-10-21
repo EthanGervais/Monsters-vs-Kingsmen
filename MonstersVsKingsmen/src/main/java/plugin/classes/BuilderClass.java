d4fdc3e30d4d741ac69a449f563ad63c2602d44d
Dawson MacPhee <dmacphee@uoguelph.ca>
2020-10-20 23:24:54 -0400

    Updated BuilderClass

--- MonstersVsKingsmen/src/main/java/plugin/classes/BuilderClass.java
+++ MonstersVsKingsmen/src/main/java/plugin/classes/BuilderClass.java
@@ -1,5 +1,17 @@
 package plugin.classes;
 
-public class BuilderClass {
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
+public class BuilderClass {
+	public void giveItems(Player player) {
+		Inventory inventory = player.getInventory();
+		ItemStack stack1 = new ItemStack(Material.COBBLESTONE, 64);
+		ItemStack stack2 = new ItemStack(Material.STONECUTTER, 1);
+		inventory.remove(Material.ENDERMAN_SPAWN_EGG);
+		inventory.addItem(stack1);
+		inventory.addItem(stack2);
+	} 
 }
