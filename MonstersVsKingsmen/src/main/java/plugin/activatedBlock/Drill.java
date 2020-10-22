da2f5a9f7d11a05c5bae85cd1547afcbc579956a
Dawson MacPhee <dmacphee@uoguelph.ca>
2020-10-21 22:17:56 -0400

    Added threads to handle drill function

--- MonstersVsKingsmen/src/main/java/plugin/activatedBlock/Drill.java
+++ MonstersVsKingsmen/src/main/java/plugin/activatedBlock/Drill.java
@@ -1,18 +1,40 @@
 package plugin.activatedBlock;
 
+import java.util.concurrent.TimeUnit;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class Drill {
 	private boolean placed = false;
 	private Inventory inv;
 	private Location loc;
+	private ItemStack stack = new ItemStack(Material.COBBLESTONE, 1);
 	
 	public Drill(Player player) {
 		inv = Bukkit.createInventory(player, 27, "Drill");
 		setBlockLocation(new Location(player.getWorld(), 0, 0, 0));
+		
+		Thread thread = new Thread() {
+			public void run() {
+				while(true) {
+					if(isPlaced()) {
+						inv.addItem(stack);
+					}
+					
+					try {
+						TimeUnit.SECONDS.sleep(2);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		thread.start();
 	}
 	
 	public void setPlaced(boolean input) {
