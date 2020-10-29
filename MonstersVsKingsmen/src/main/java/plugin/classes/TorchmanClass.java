53d79fdc5bbdcc8aebb901b101278c0f691fc3e0
EthanGervais <EthanGervais@users.noreply.github.com>
2020-10-28 20:25:05 -0400

    Updated Torchman Class
    Gives the torchman their items.

--- MonstersVsKingsmen/src/main/java/plugin/classes/TorchmanClass.java
+++ MonstersVsKingsmen/src/main/java/plugin/classes/TorchmanClass.java
@@ -1,5 +1,30 @@
 package plugin.classes;
 
-public class TorchmanClass {
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+public class TorchmanClass {
+	public void giveItems(Player player) {
+		Inventory inventory = player.getInventory();
+		ItemStack axeStack = new ItemStack(Material.DIAMOND_AXE, 1);
+		ItemMeta meta = axeStack.getItemMeta();
+		meta.setDisplayName("Torchman's Axe");
+		axeStack.setItemMeta(meta);
+		
+		inventory.remove(Material.TROPICAL_FISH_SPAWN_EGG);
+		inventory.addItem(axeStack);
+	}
+	
+//	public void onBlockBreak(BlockBreakEvent event) {
+//		if (!(event instanceof BlockBreakEvent)) {
+//			return;
+//		}
+//		
+//		if (event.getBlock().getType() == Material.OAK_WOOD) {
+//			
+//		}
+//	}
 }
