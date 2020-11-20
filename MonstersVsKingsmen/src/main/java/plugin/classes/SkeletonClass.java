af520825997370387f17ec1446a6ab3a7e55eed0
EthanGervais <EthanGervais@users.noreply.github.com>
2020-11-19 22:33:29 -0500

    Updated ZombieClass and SkeletonClass
    Implemented the super class MonsterClass.

--- MonstersVsKingsmen/src/main/java/plugin/classes/SkeletonClass.java
+++ MonstersVsKingsmen/src/main/java/plugin/classes/SkeletonClass.java
@@ -1,5 +1,7 @@
 package plugin.classes;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -9,7 +11,37 @@ import org.bukkit.inventory.ItemStack;
 import me.libraryaddict.disguise.disguisetypes.DisguiseType;
 import me.libraryaddict.disguise.disguisetypes.MobDisguise;
 
-public class SkeletonClass {
+public class SkeletonClass extends MonsterClass {
+	
+	public SkeletonClass() {
+		super();
+		ItemStack bowStack = new ItemStack(Material.BOW);
+		ItemStack arrowsStack = new ItemStack(Material.ARROW, 256);
+		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+		items.add(bowStack);
+		items.add(arrowsStack);
+		super.setItems(items);
+		
+		super.setDisguise(DisguiseType.SKELETON);
+	}
+	
+	public void setClass(Player player) {
+		super.setPlayer(player);
+		super.spawnMonster();
+		
+		ItemStack[] armorStack = new ItemStack[4];
+		armorStack[0] = new ItemStack(Material.LEATHER_BOOTS);
+		armorStack[1] = new ItemStack(Material.LEATHER_LEGGINGS);
+		armorStack[2] = new ItemStack(Material.LEATHER_CHESTPLATE);
+		armorStack[3] = new ItemStack(Material.LEATHER_HELMET);
+		
+		for (int i = 0; i < armorStack.length; i++) {
+			armorStack[i].addEnchantment(Enchantment.BINDING_CURSE, 1);
+		}
+		
+		player.getInventory().setArmorContents(armorStack);
+	}
+	
 	public void giveItems(Player player) {
 		MobDisguise disguise = new MobDisguise(DisguiseType.SKELETON);
 		disguise.setEntity(player);
--- MonstersVsKingsmen/src/main/java/plugin/classes/ZombieClass.java
+++ MonstersVsKingsmen/src/main/java/plugin/classes/ZombieClass.java
@@ -1,5 +1,7 @@
 package plugin.classes;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -9,7 +11,35 @@ import org.bukkit.inventory.ItemStack;
 import me.libraryaddict.disguise.disguisetypes.DisguiseType;
 import me.libraryaddict.disguise.disguisetypes.MobDisguise;
 
-public class ZombieClass {
+public class ZombieClass extends MonsterClass {
+	
+	public ZombieClass() {
+		super();
+		ItemStack swordStack = new ItemStack(Material.IRON_SWORD);
+		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+		items.add(swordStack);
+		super.setItems(items);
+		
+		super.setDisguise(DisguiseType.ZOMBIE);
+	}
+	
+	public void setClass(Player player) {
+		super.setPlayer(player);
+		super.spawnMonster();
+		
+		ItemStack[] armorStack = new ItemStack[4];
+		armorStack[0] = new ItemStack(Material.IRON_BOOTS);
+		armorStack[1] = new ItemStack(Material.IRON_LEGGINGS);
+		armorStack[2] = new ItemStack(Material.IRON_CHESTPLATE);
+		armorStack[3] = new ItemStack(Material.IRON_HELMET);
+		
+		for (int i = 0; i < armorStack.length; i++) {
+			armorStack[i].addEnchantment(Enchantment.BINDING_CURSE, 1);
+		}
+		
+		player.getInventory().setArmorContents(armorStack);
+	}
+	
 	public void giveItems(Player player) {
 		MobDisguise disguise = new MobDisguise(DisguiseType.ZOMBIE);
 		disguise.setEntity(player);
